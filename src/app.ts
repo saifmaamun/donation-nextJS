c1f64a95c5d288ed82035b62c3dadab23b20c1ea
Saif Maamun <sadifmamun@gmail.com>
2024-02-07 03:43:17 +0600

    user backend  --DONE

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,9 @@ import express from "express";
 import cors from "cors";
 const app = express();
 
+// routes
+import userRouter from "./app/modules/user/user.routes";
+
 app.use(cors());
 // parser
 app.use(express.json());
@@ -10,5 +13,8 @@ app.use(express.urlencoded({ extended: true }));
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
+
+// users route
+app.use("/api/v1/users", userRouter);
 
 export default app;
